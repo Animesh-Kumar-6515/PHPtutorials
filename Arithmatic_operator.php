b11898ba77c916d35633a6d9f704c302a052ffaa
Animesh-Kumar-6515 <81911506+Animesh-Kumar-6515@users.noreply.github.com>
2021-07-27 12:22:12 +0530

    Add files via upload

---
+++ Arithmatic_operator.php
@@ -0,0 +1,22 @@
+<?php
+$a=10;
+$b=3;
+$c=$a+$b;
+echo $c;
+echo "<br>";
+$c=$a-$b;
+echo $c;
+echo "<br>";
+$c=$a*$b;
+echo $c;
+echo "<br>";
+$c=$a/$b;
+echo $c;
+echo "<br>";
+$c=$a%$b;
+echo $c;
+echo "<br>";
+$c=$a**$b;
+echo $c;
+
+?>
