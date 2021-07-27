5a32602454224f7e8e8e792be064a5f42c53ca1b
Animesh-Kumar-6515 <81911506+Animesh-Kumar-6515@users.noreply.github.com>
2021-07-27 12:23:40 +0530

    Add files via upload

---
+++ constant_variable.php
@@ -0,0 +1,8 @@
+<?php
+// define(name,value,case-insensitive)
+// this is used to take fixed varaible constant i.e. immutable
+define("num",500);
+echo num;
+$sum=test+50;
+echo $sum;
+?>
---
+++ datatype.php
@@ -0,0 +1,16 @@
+<?php
+#string data type
+$x="Animesh Kumar";
+echo $x."<br>";
+var_dump($x);
+echo "<br>";
+#int data type
+$n=6515;
+echo $n."<br>";
+var_dump($n);
+echo "<br>";
+#boolean  data type
+$b=false;
+echo $b. "<br>";
+var_dump($b);
+?>
